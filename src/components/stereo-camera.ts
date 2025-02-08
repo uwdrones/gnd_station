698dd6ac4723d5d13d6221c0b9bbbf3fd2521614
Angad Wadhwa <angadw@uw.edu>
2025-02-08 15:16:28 -0800

    Update stereo-camera.ts

--- src/components/stereo-camera.ts
+++ src/components/stereo-camera.ts
@@ -1,38 +1,63 @@
 import { Injectable } from '@nestjs/common';
-import { exec } from 'child_process';
-import {leftPipeline, rightPipeline} from 'src/utils/constants';
+import { exec, ChildProcessWithoutNullStreams } from 'child_process';
+import { leftPipeline, rightPipeline } from 'src/utils/constants';
 
 @Injectable()
 export class StreamService {
-  
- public startBothCameras() {
+  private leftCameraProcess: ChildProcessWithoutNullStreams;
+  private rightCameraProcess: ChildProcessWithoutNullStreams;
+
+  public startBothCameras() {
     this.startLeftCamera();
     this.startRightCamera();
   }
 
   public startLeftCamera() {
-    const process = exec(`gst-launch-1.0 ${this.leftPipeline}`);
-    process.stdout.on('data', (data) => {
+    this.leftCameraProcess = exec(`gst-launch-1.0 ${this.leftPipeline}`);
+    this.leftCameraProcess.stdout.on('data', (data) => {
       console.log('Left Camera Stream Output:', data);
     });
-    process.stderr.on('data', (data) => {
+    this.leftCameraProcess.stderr.on('data', (data) => {
       console.error('Left Camera Stream Error:', data);
     });
-    process.on('close', (code) => {
+    this.leftCameraProcess.on('close', (code) => {
       console.log('Left Camera Stream Process Closed with Code:', code);
     });
   }
 
   public startRightCamera() {
-    const process = exec(`gst-launch-1.0 ${this.rightPipeline}`);
-    process.stdout.on('data', (data) => {
+    this.rightCameraProcess = exec(`gst-launch-1.0 ${this.rightPipeline}`);
+    this.rightCameraProcess.stdout.on('data', (data) => {
       console.log('Right Camera Stream Output:', data);
     });
-    process.stderr.on('data', (data) => {
+    this.rightCameraProcess.stderr.on('data', (data) => {
       console.error('Right Camera Stream Error:', data);
     });
-    process.on('close', (code) => {
+    this.rightCameraProcess.on('close', (code) => {
       console.log('Right Camera Stream Process Closed with Code:', code);
     });
+  }
+
+  public stopBothCameras() {
+    this.stopLeftCamera();
+    this.stopRightCamera();
+  }
+
+  public stopLeftCamera() {
+    if (this.leftCameraProcess) {
+      this.leftCameraProcess.kill();
+      console.log('Left Camera Stream Stopped');
+    } else {
+      console.log('Left Camera Stream is not running');
+    }
+  }
+
+  public stopRightCamera() {
+    if (this.rightCameraProcess) {
+      this.rightCameraProcess.kill();
+      console.log('Right Camera Stream Stopped');
+    } else {
+      console.log('Right Camera Stream is not running');
+    }
   }
 }
