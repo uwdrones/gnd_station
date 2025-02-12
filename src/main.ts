452b0c134431fdf97494c1ebde126af893b4cfe6
Perry Chien <peiruchien1@gmail.com>
2025-02-11 19:37:20 -0800

    better err handling as per eslint

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+  } catch (err) {
+    console.error('Error starting app:', err);
+    process.exit(1);
+  }
 }
 bootstrap();
