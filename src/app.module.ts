11d98f366bfaef67d48291c5b7acf191843b97e6
Perry Chien <peiruchien1@gmail.com>
2025-02-11 19:48:22 -0800

    finalized proj setup (commonjs more stable)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DroneGateway } from './gateway/drone-gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DroneGateway],
 })
 export class AppModule {}
