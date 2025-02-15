8ebf036ede1fe296a30ae4ebc22db8dff92c112a
Angad Wadhwa <angadw@uw.edu>
2025-02-15 13:23:27 -0800

    Update stereo-stream-service.ts
    Updated to use broadcastService instead to relay data.

--- src/services/vision/stereo-stream-service.ts
+++ src/services/vision/stereo-stream-service.ts
@@ -1,48 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { exec, ChildProcessWithoutNullStreams } from 'child_process';
-import * as WebSocket from 'ws';
-import {leftPipeline, rightPipeline} from 'src/utils/constants';
-import { WebSocketModule } from 'src/services/websocket.module';
+import { leftPipeline, rightPipeline } from 'src/utils/constants';
+import { BroadcastService } from './broadcast.service';
 
 @Injectable()
 export class StreamService {
   private leftCameraProcess: ChildProcessWithoutNullStreams;
   private rightCameraProcess: ChildProcessWithoutNullStreams;
-  private wss: WebSocket.Server;
-  private clients: Set<WebSocket> = new Set();
-  
-  constructor(private readonly webSocketModule: WebSocketModule) {
-    this.webSocketModule.wss.on('connection', (ws) => {
-      console.log('Client connected');
-      this.clients.add(ws);
-      ws.on('close', () => {
-        console.log('Client disconnected');
-        this.clients.delete(ws);
-      });
 
-      ws.on('error', (error) => {
-        console.error('WebSocket error:', error);
-        this.clients.delete(ws);
-      });
-    });
-  }
+  constructor(private readonly broadcastService: BroadcastService) {}
 
   public startBothCameras() {
     this.startLeftCamera();
     this.startRightCamera();
   }
-  
+
   public startLeftCamera() {
     this.leftCameraProcess = exec(`gst-launch-1.0 ${leftPipeline}`);
-
     this.leftCameraProcess.stdout.on('data', (data) => {
-      this.broadcastFrame(data);
+      this.broadcastService.broadcast(
+        data,
+        { type: 'frame', source: 'left' }
+      );
     });
-
     this.leftCameraProcess.stderr.on('data', (data) => {
       console.error('Left Camera Stream Error:', data.toString());
     });
-
     this.leftCameraProcess.on('close', (code) => {
       console.log('Left Camera Stream Process Closed with Code:', code);
     });
@@ -50,15 +33,15 @@ export class StreamService {
 
   public startRightCamera() {
     this.rightCameraProcess = exec(`gst-launch-1.0 ${rightPipeline}`);
-
     this.rightCameraProcess.stdout.on('data', (data) => {
-      this.broadcastFrame(data);
+      this.broadcastService.broadcast(
+        data,
+        { type: 'frame', source: 'right' }
+      );
     });
-
     this.rightCameraProcess.stderr.on('data', (data) => {
       console.error('Right Camera Stream Error:', data.toString());
     });
-
     this.rightCameraProcess.on('close', (code) => {
       console.log('Right Camera Stream Process Closed with Code:', code);
     });
@@ -84,14 +67,6 @@ export class StreamService {
       console.log('Right Camera Stream Stopped');
     } else {
       console.log('Right Camera Stream is not running');
-    }
-  }
-
-  private broadcastFrame(frame: Buffer) {
-    for (const client of this.clients) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(frame);
-      }
     }
   }
 }
