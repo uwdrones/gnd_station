7abe62205998ba093fcb843062acabe9d58a6135
Angad Wadhwa <angadw@uw.edu>
2025-02-22 13:52:41 -0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
   try {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    app.useStaticAssets(join(__dirname, '..', 'public'));
+    
     await app.listen(3000);
   } catch (err) {
     console.error('Error starting app:', err);
