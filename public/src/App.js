4dc8906fd7bf39be3d4f7b0f67ea16113e148eea
Angad Wadhwa <angadw@uw.edu>
2025-02-22 13:40:31 -0800

    Create App.js
    Combining react.js App with Nest.js project.

---
+++ public/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloWorld from './components/HelloWorld';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+}
+export default App;
