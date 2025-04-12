0b03eaee543b279cf1ea10f18d904dde657191c5
Perry Chien <peiruchien1@gmail.com>
2025-04-12 14:29:17 -0700

    new frontend app viz structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,41 @@
 import React from "react";
-import HelloWorld from "./components/HelloWorld";
 import "./App.css";
-
+import { DroneProvider } from "./context/DroneContext";
+import Telemetry from "./components/telemetry/Telemetry";
+import SystemMonitor from "./components/system-monitor/SystemMonitor";
+import Controls from "./components/controls/Controls";
+import Camera from "./components/camera/Camera";
 
 const App = () => {
   return (
-    <div>
-      <HelloWorld />
-    </div>
+    <DroneProvider>
+      <div className="app-container">
+        <header className="app-header">
+          <h1>Drone Ground Station</h1>
+        </header>
+        
+        <main className="app-content">
+          <div className="dashboard-layout">
+            <div className="dashboard-row">
+              <div className="dashboard-col">
+                <Telemetry />
+              </div>
+              <div className="dashboard-col">
+                <SystemMonitor />
+              </div>
+            </div>
+            <div className="dashboard-row">
+              <div className="dashboard-col">
+                <Controls />
+              </div>
+              <div className="dashboard-col">
+                <Camera />
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </DroneProvider>
   );
 };
 
